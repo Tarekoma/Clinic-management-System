901c95edf071f2b070f6fe235d7774ea20993bfe
Tarek Omar <162827740+Tarekoma@users.noreply.github.com>
2026-08-10 14:21:20 +0300

    Delete App/lib directory

--- App/lib/providers/admin_providers.dart
+++
@@ -1,9 +0,0 @@
-// lib/providers/admin_providers.dart
-
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:Hakim/viewmodels/admin_viewmodel.dart';
-
-final adminViewModelProvider =
-    StateNotifierProvider<AdminViewModel, AdminState>(
-      (ref) => AdminViewModel(),
-    );
